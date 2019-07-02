ff6d1a1ce9efe75fcdb1c80eb7ae6d3f2f45a265
Sebastián Berlati <sberlati@gmail.com>
2019-07-02 16:17:35 -0300

    Escribir/leer/mostrar logs de la aplicación

--- www/assets/js/main.js
+++ www/assets/js/main.js
@@ -1,19 +1,55 @@
 var app = {
     init: function() {
         this.asignoEventos();
+        this.renderLog();
     },
 
     asignoEventos: function() {
+        var _this = this;
+        this.log("App. iniciada");
         document.addEventListener('deviceready', function() {
-           this.initBackgroundMode();
+           _this.initBackgroundMode();
         }, false);
+        $('.log-clear').click(function(e) {
+            e.preventDefault();
+            _this.clearLogs();
+        });
     },
 
     initBackgroundMode: function() {
+        this.log("BackgroundMode iniciado");
         cordova.plugins.backgroundMode.setDefaults({
             title: 'Agenda remota',
             text: 'Funcionando en segundo plano...'
         });
         cordova.plugins.backgroundMode.setEnabled(true);
+    },
+
+    retrieveLog: function() {
+        return JSON.parse(localStorage.getItem('appLogs'));
+    },
+
+    renderLog: function() {
+        $('.log-entries').empty();
+        var log = this.retrieveLog();
+        for(var i=0;i<log.length;i++) {
+            $('.log-entries').prepend('<p class="entry">'+log[i]+'</p>');
+        }
+    },
+
+    log: function(message) {
+        var msg         = '['+moment().format('d/m/Y HH:mm:ss').toString()+'] ' + message;
+        var logString   = localStorage.getItem('appLogs');
+        var _log        = ((logString == null || logString == "") ? [] : JSON.parse(logString));
+        _log.push(msg);
+        localStorage.setItem('appLogs', JSON.stringify(_log));
+        this.renderLog();
+    },
+
+    clearLogs: function() {
+        localStorage.setItem('appLogs', "[]");
+        this.log("Reportes y seguimiento vaciados.");
+        this.renderLog();
     }
 };
+app.init();
